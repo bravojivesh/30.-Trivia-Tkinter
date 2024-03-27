26322bcf8f43dd100a7a147e287bd18ac198efc0
JIVESH HAMAL <bravojivesh@gmail.com>
2024-03-28 02:16:55 +0530

    second commit

--- question_model.py
+++ question_model.py
@@ -1,5 +1,6 @@
+import html
 class Question:
 
     def __init__(self, q_text, q_answer):
-        self.text = q_text
+        self.text = html.unescape(q_text) #ang did it in quiz_brain, but I am doing it here.
         self.answer = q_answer
